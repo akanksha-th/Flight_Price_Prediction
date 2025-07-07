e325c48a85c3b33455d889f3ceddc07add981fac
Akanksha <ak.t.kree@gmail.com>
2025-07-07 17:37:44 +0530

    added remote files

---
+++ setup.py
@@ -0,0 +1,13 @@
+import opendatasets as od
+import os
+
+url = 'https://www.kaggle.com/datasets/shubhambathwal/flight-price-prediction'
+data_dir = '/data'
+
+# Ensure the directory exists
+os.makedirs(data_dir, exist_ok=True)
+
+# Download dataset into the specified directory
+od.download(dataset_id_or_url=url, data_dir=data_dir)
+
+od.download()
